aee8af99630c79604e974a8fb843c900de95ca68
zer0VAns <nsilvahollger@gmail.com>
2026-07-24 21:54:01 -0300

    added basic SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  site: "https://netsh-digitalagency.onrender.com",
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
